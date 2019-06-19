a362cc677881d4e1f988d216f068ebc12e25b7e4
Roman Tolstoi <roman_tolstoi@epam.com>
2019-06-19 19:15:46 +0300

    Updates

---
+++ precache-manifest.4b773cc819a2020e02555ebe6ad55f53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34862a66d27c4e62476acdd38172a782",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "9df5c9135c6881017aed",
+    "url": "/contact-manager/static/css/main.445a5d26.chunk.css"
+  },
+  {
+    "revision": "55e005b0560030501a6c",
+    "url": "/contact-manager/static/js/2.2a7ae057.chunk.js"
+  },
+  {
+    "revision": "9df5c9135c6881017aed",
+    "url": "/contact-manager/static/js/main.f51bcdb2.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
